b0fbc586189226a6c292aeb4b9238b506e59f47e
adesm <ademugni893@gmail.com>
2026-09-17 11:26:15 +0700

    feat:  home page

---
+++ components/SubsCard.tsx
@@ -0,0 +1,106 @@
+import clsx from "clsx";
+import { Image, Pressable, Text, View } from "react-native";
+import {
+    formatCurrency,
+    formatStatusLabel,
+    formatSubscriptionDateTime,
+} from "../lib/utils";
+
+const SubsCard = ({
+  name,
+  price,
+  currency,
+  icon,
+  billing,
+  color,
+  category,
+  plan,
+  renewalDate,
+  expanded,
+  onPress,
+  paymentMethod,
+  startDate,
+  status,
+}: SubscriptionCardProps) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      className={clsx("sub-card", expanded ? "sub-card-expanded" : "bg-card")}
+      style={!expanded && color ? { backgroundColor: color } : undefined}
+    >
+      <View className="sub-head">
+        <View className="sub-main">
+          <Image source={icon} className="sub-icon" />
+          <View className="sub-copy">
+            <Text numberOfLines={1} className="sub-title">
+              {name}
+            </Text>
+            <Text numberOfLines={1} ellipsizeMode="tail" className="sub-meta">
+              {category?.trim() ||
+                plan?.trim() ||
+                (renewalDate ? formatSubscriptionDateTime(renewalDate) : "")}
+            </Text>
+          </View>
+        </View>
+
+        <View className="sub-price-box">
+          <Text className="sub-price">{formatCurrency(price, currency)}</Text>
+          <Text className="sub-billing">{billing}</Text>
+        </View>
+      </View>
+
+      {expanded && (
+        <View className="sub-bdy">
+          <View className="sub-details">
+            <View className="sub-row">
+              <View className="sub-row-copy">
+                <Text className="sub-label">Payment:</Text>
+                <Text className="sub-value" numberOfLines={1}>
+                  {paymentMethod?.trim()}
+                </Text>
+              </View>
+            </View>
+
+            <View className="sub-row">
+              <View className="sub-row-copy">
+                <Text className="sub-label">Category:</Text>
+                <Text className="sub-value" numberOfLines={1}>
+                  {category?.trim() || plan?.trim()}
+                </Text>
+              </View>
+            </View>
+
+            <View className="sub-row">
+              <View className="sub-row-copy">
+                <Text className="sub-label">Started:</Text>
+                <Text className="sub-value" numberOfLines={1}>
+                  {startDate ? formatSubscriptionDateTime(startDate) : ""}
+                </Text>
+              </View>
+            </View>
+
+            <View className="sub-row">
+              <View className="sub-row-copy">
+                <Text className="sub-label">Renewal Date:</Text>
+                <Text className="sub-value" numberOfLines={1}>
+                  {renewalDate ? formatSubscriptionDateTime(renewalDate) : ""}
+                </Text>
+              </View>
+            </View>
+
+            <View className="sub-row">
+              <View className="sub-row-copy">
+                <Text className="sub-label">Renewal Date:</Text>
+                <Text className="sub-value" numberOfLines={1}>
+                  {status ? formatStatusLabel(status) : ""}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      )}
+    </Pressable>
+  );
+};
+
+export default SubsCard;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,29 +1,87 @@
-import { Link } from "expo-router";
+import dayjs from "dayjs";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { useState } from "react";
+import { FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import ListHeading from "../../../components/ListHeading";
+import SubsCard from "../../../components/SubsCard";
+import UpcommingSubsCard from "../../../components/UpcommingSubsCard";
+import {
+  HOME_BALANCE,
+  HOME_SUBSCRIPTIONS,
+  HOME_USER,
+  UPCOMING_SUBSCRIPTIONS,
+} from "../../../constants/data";
+import { icons } from "../../../constants/icons";
+import images from "../../../constants/images";
+import { formatCurrency } from "../../../lib/utils";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
+  const [expandedSubsId, setExpandedSubsId] = useState<string | null>(null);
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-7xl font-sans-extrabold text-primary">home</Text>
-      <Link href="/onboarding" className="mt-4 bg-primary text-white p-4">
-        Go to oonboarding
-      </Link>
-      <Link href="/(auth)/sign-in" className="mt-4 bg-primary text-white p-4">
-        sign in
-      </Link>
+      <FlatList
+        ListHeaderComponent={() => (
+          <>
+            <View className="home-header">
+              <View className="home-user">
+                <Image source={images.avatar} className="home-avatar" />
+                <Text className="home-user-name">{HOME_USER.name}</Text>
+              </View>
 
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "valude" },
-        }}
-      >
-        Claude
-      </Link>
+              <Image source={icons.add} className="home-add-icon" />
+            </View>
+
+            <View className="home-balance-card">
+              <Text className="home-balance-label">Balance</Text>
+              <View className="home-balance-row">
+                <Text className="home-balance-amount">
+                  {formatCurrency(HOME_BALANCE.amount)}
+                </Text>
+                <Text className="home-balance-date">
+                  {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+                </Text>
+              </View>
+            </View>
+
+            <View className="mb-5">
+              <ListHeading title="Upcoming" />
+              <FlatList
+                data={UPCOMING_SUBSCRIPTIONS}
+                renderItem={({ item }) => <UpcommingSubsCard {...item} />}
+                keyExtractor={(item) => item.id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                ListEmptyComponent={
+                  <Text className="home-empty-state">No data</Text>
+                }
+              />
+            </View>
+
+            <ListHeading title="All Subs" />
+          </>
+        )}
+        data={HOME_SUBSCRIPTIONS}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <SubsCard
+            expanded={expandedSubsId === item.id}
+            onPress={() =>
+              setExpandedSubsId((current) =>
+                current === item.id ? null : item.id,
+              )
+            }
+            {...item}
+          />
+        )}
+        extraData={expandedSubsId}
+        ItemSeparatorComponent={() => <View className="h-5" />}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={<Text className="home-empty-state">no data</Text>}
+        contentContainerClassName="pb-20"
+      />
     </SafeAreaView>
   );
 }
